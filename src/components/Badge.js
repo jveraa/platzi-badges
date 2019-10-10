7c001a57f08308ac132c2cf2be8087bc983a4ad0
jveraa <jmva271008@gmail.com>
2019-10-10 12:23:55 -0500

    Se agrega comentarios como guia de aprendizaje.

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,24 +2,28 @@ import React from 'react';
 
 import confLogo from '../images/badge-header.svg';
 
+//El componente es una clase
+//En el img al hacer referencia a la imagen, tb traera el hash de la imagen que lo guardara en cache
+//para que no se este cargando a cada rato.
+
 class Badge extends React.Component {
   render() {
     return (
       <div>
         <div>
-          <img src={confLogo} alt="Logo de la conferencia" />
+          <img src={confLogo} alt="Logo de la conferencia" /> 
         </div>
 
         <div>
           <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
           <h1>
-            Richard <br /> Kaufman
+            Jorge <br /> Vera
           </h1>
         </div>
 
         <div>
           <p>Frontend Engineer</p>
-          <p>@sparragus</p>
+          <p>@jveraa</p>
         </div>
 
         <div>#platziconf</div>
@@ -28,4 +32,4 @@ class Badge extends React.Component {
   }
 }
 
-export default Badge;
+export default Badge; //exportamos porque estamos en otro archivo
